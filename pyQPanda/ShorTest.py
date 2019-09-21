49e823721334ec9ea9965d0d4547a136d67c93fc
369038080 <369038080@qq.com>
2019-09-21 16:58:18 +0800

    <feat>:pyqpanda添加shor、chemiq、gradient、NoGradient

---
+++ pyQPanda/ShorTest.py
@@ -0,0 +1,316 @@
+from pyqpanda import *
+import matplotlib.pyplot as plt
+import math as m 
+
+def plotBar(xdata, ydata):
+    fig, ax = plt.subplots()
+    fig.set_size_inches(6,6)
+    fig.set_dpi(100)
+    
+    rects =  ax.bar(xdata, ydata, color='b')
+
+    for rect in rects:
+        height = rect.get_height()
+        plt.text(rect.get_x() + rect.get_width() / 2, height, str(height), ha="center", va="bottom")
+    
+    plt.rcParams['font.sans-serif']=['Arial']
+    plt.title("Origin Q", loc='right', alpha = 0.5)
+    plt.ylabel('Times')
+    plt.xlabel('States')
+        
+    plt.show()
+    
+def reorganizeData(measure_qubits, quick_meausre_result):
+    xdata = []
+    ydata = []
+
+    for i in quick_meausre_result:
+        xdata.append(i)
+        ydata.append(quick_meausre_result[i])
+    
+    return xdata, ydata
+   
+def gcd(m,n):
+    if not n:
+        return m
+    else:
+        return gcd(n, m%n)
+  
+def MAJ(a, b, c):
+    circ = QCircuit()
+    circ.insert(CNOT(c,b))
+    circ.insert(CNOT(c,a))
+    circ.insert(Toffoli(a, b, c))
+
+    return circ
+	
+def Adder(a, b, c):
+    circuit = CreateEmptyCircuit()
+    nbit = len(a)
+    circuit.insert(MAJ(c, a[0], b[0]))
+    for i in range(1,nbit,1):
+        circuit.insert(MAJ(b[i - 1], a[i], b[i]))
+    for i in range(nbit-1,0, - 1):
+        circuit.insert(MAJ(b[i - 1], a[i], b[i]))
+    circuit.insert(UMA(c, a[0], b[0]))
+    return circuit
+    
+def UMA(a, b, c):
+    circ = QCircuit()
+    circ.insert(Toffoli(a, b, c)).insert(CNOT(c, a)).insert(CNOT(a, b))  
+
+    return circ   
+    
+def MAJ2(a, b, c):
+    if ((len(a) == 0) or (len(a) != (len(b)))):
+        raise RuntimeError('a and b must be equal, but not equal to 0!')
+
+    nbit = len(a)
+    circ = QCircuit()
+    circ.insert(MAJ(c, a[0], b[0]))
+
+    for i in range(1, nbit):
+        circ.insert(MAJ(b[i-1], a[i], b[i]))
+    
+    return circ
+    
+    if ((len(a) == 0) or (len(a) != (len(b)))):
+        raise RuntimeError('a and b must be equal, but not equal to 0!')
+
+    nbit = len(a)
+    circ = QCircuit()
+    circ.insert(MAJ(c, a[0], b[0]))
+
+    for i in range(1, nbit):
+        circ.insert(MAJ(b[i-1], a[i], b[i]))
+
+    for i in range(nbit-1, 0, -1):
+        circ.insert(UMA(b[i-1], a[i], b[i]))
+
+    circ.insert(UMA(c, a[0], b[0]))
+    
+    return circ
+    
+    if ((len(a) == 0) or (len(a) != (len(b)))):
+        raise RuntimeError('a and b must be equal, but not equal to 0!')
+
+    circ = QCircuit()
+
+    circ.insert(MAJ2(a, b, c))
+    circ.insert(CNOT(b[-1], carry))
+    circ.insert(MAJ2(a, b, c).dagger())
+
+    return circ
+    
+def bindData(qlist, data):
+    check_value = 1 << len(qlist)
+    if (data >= check_value):
+        raise RuntimeError('data >= check_value')
+
+    circ = QCircuit()
+    i = 0
+    while (data >= 1):
+        if (data % 2) == 1:
+            circ.insert(X(qlist[i]))
+        
+        data = data >> 1
+        i = i+1
+    
+    return circ
+
+def isCarry(a, b, c, carry):
+    if ((len(a) == 0) or (len(a) != (len(b)))):
+        raise RuntimeError('a and b must be equal, but not equal to 0!')
+
+    circ = QCircuit()
+
+    circ.insert(MAJ2(a, b, c))
+    circ.insert(CNOT(b[-1], carry))
+    circ.insert(MAJ2(a, b, c).dagger())
+
+    return circ
+    
+def constModAdd(qa, C, M, qb, qs1):
+    circ = QCircuit()
+    
+    q_num = len(qa)
+
+    tmp_value = (1 << q_num) - M + C
+
+    circ.insert(bindData(qb, tmp_value))
+    circ.insert(isCarry(qa, qb, qs1[1], qs1[0]))
+    circ.insert(bindData(qb, tmp_value))
+    
+    tmp_circ = QCircuit()
+    tmp_circ.insert(bindData(qb, tmp_value))
+    tmp_circ.insert(Adder(qa, qb, qs1[1]))
+    tmp_circ.insert(bindData(qb, tmp_value))
+    tmp_circ = tmp_circ.control([qs1[0]])
+    circ.insert(tmp_circ)
+
+    circ.insert(X(qs1[0]))
+
+    tmp2_circ = QCircuit()
+    tmp2_circ.insert(bindData(qb, C))
+    tmp2_circ.insert(Adder(qa, qb, qs1[1]))
+    tmp2_circ.insert(bindData(qb, C))
+    tmp2_circ = tmp2_circ.control([qs1[0]])
+    circ.insert(tmp2_circ)
+
+    circ.insert(X(qs1[0]))
+
+    tmp_value = (1 << q_num) - C
+    circ.insert(bindData(qb, tmp_value))
+    circ.insert(isCarry(qa, qb, qs1[1], qs1[0]))
+    circ.insert(bindData(qb, tmp_value))
+    circ.insert(X(qs1[0]))
+
+    return circ
+    
+def modreverse(c, m):
+    if (c == 0):
+        raise RecursionError('c is zero!')
+    
+    if (c == 1):
+        return 1
+    
+    m1 = m
+    quotient = []
+    quo = m // c
+    remainder = m % c
+
+    quotient.append(quo)
+
+    while (remainder != 1):
+        m = c
+        c = remainder
+        quo = m // c
+        remainder = m % c
+        quotient.append(quo)
+
+    if (len(quotient) == 1):
+        return m - quo
+
+    if (len(quotient) == 2):
+        return 1 + quotient[0]*quotient[1]
+
+    rev1 = 1
+    rev2 = quotient[-1]
+    reverse_list = quotient[0:-1]
+    reverse_list.reverse()
+    for i in reverse_list:
+        rev1 = rev1 + rev2 * i
+        temp = rev1
+        rev1 = rev2
+        rev2 = temp
+
+    if ((len(quotient) % 2) == 0):
+        return rev2
+
+    return m1 - rev2
+    
+
+def constModMul(qa, const_num, M, qs1, qs2, qs3):
+    circ = QCircuit()
+    
+    q_num = len(qa)
+
+    for i in range(0, q_num):
+        tmp_circ = QCircuit()
+        tmp = const_num * pow(2, i) %M
+        tmp_circ.insert(constModAdd(qs1, tmp, M, qs2, qs3))
+        tmp_circ = tmp_circ.control([qa[i]])
+        circ.insert(tmp_circ)
+
+    #state swap
+    for i in range(0, q_num):
+        circ.insert(CNOT(qa[i], qs1[i]))
+        circ.insert(CNOT(qs1[i], qa[i]))
+        circ.insert(CNOT(qa[i], qs1[i]))
+
+    Crev = modreverse(const_num, M)
+    
+    tmp2_circ = QCircuit()
+    for i in range(0, q_num):
+        tmp = Crev* pow(2, i)
+        tmp = tmp % M
+        tmp_circ = QCircuit()
+        tmp_circ.insert(constModAdd(qs1, tmp, M, qs2, qs3))
+        tmp_circ = tmp_circ.control([qa[i]])
+        tmp2_circ.insert(tmp_circ)
+    
+    circ.insert(tmp2_circ.dagger())
+
+    return circ
+    
+def constModExp(qa, qb, base, M, qs1, qs2, qs3):
+    circ = QCircuit()
+
+    cqnum = len(qa)
+
+    temp = base
+
+    for i in range(0, cqnum):    
+        circ.insert(constModMul(qb, temp, M, qs1, qs2, qs3).control([qa[i]]))
+        temp = temp * temp
+        temp = temp % M
+
+    return circ
+    
+def qft(qlist):
+    circ = QCircuit()
+    
+    qnum = len(qlist)
+    for i in range(0, qnum):
+        circ.insert(H(qlist[qnum-1-i]))
+        for j in range(i + 1, qnum):
+            circ.insert(CR(qlist[qnum-1-j], qlist[qnum-1-i], m.pi/(1 << (j-i))))
+
+    for i in range(0, qnum//2):
+        circ.insert(CNOT(qlist[i], qlist[qnum-1-i]))
+        circ.insert(CNOT(qlist[qnum-1-i], qlist[i]))
+        circ.insert(CNOT(qlist[i], qlist[qnum-1-i]))
+
+    return circ
+
+def shorAlg(base, M):
+    if ((base < 2) or (base > M - 1)):
+        raise('Invalid base!')
+
+    if (gcd(base, M) != 1):
+        raise('Invalid base! base and M must be mutually prime')
+    
+    binary_len = 0
+    while M >> binary_len != 0 :
+        binary_len = binary_len + 1
+    
+    machine = init_quantum_machine(QMachineType.CPU_SINGLE_THREAD)
+
+    qa = machine.qAlloc_many(binary_len*2)
+    qb = machine.qAlloc_many(binary_len)
+
+    qs1 = machine.qAlloc_many(binary_len)
+    qs2 = machine.qAlloc_many(binary_len)
+    qs3 = machine.qAlloc_many(2)
+
+    prog = QProg()
+
+    prog.insert(X(qb[0]))
+    prog.insert(single_gate_apply_to_all(H, qa))
+    prog.insert(constModExp(qa, qb, base, M, qs1, qs2, qs3))
+    prog.insert(qft(qa).dagger())
+
+    directly_run(prog)
+    result = quick_measure(qa, 100)
+
+    print(result)
+
+    xdata, ydata = reorganizeData(qa, result)
+    plotBar(xdata, ydata)
+
+    return result
+    
+if __name__=="__main__":
+    base = 2
+    N = 21
+    shorAlg(base, N)
---
+++ pyQPanda/VQE_NoGradient.py
@@ -0,0 +1,309 @@
+from pyqpanda import *
+from psi4_wrapper import *
+import numpy as np
+from functools import partial
+from math import pi
+import matplotlib.pyplot as plt
+#损失函数
+def loss_func(para_list, qubit_number, electron_number, Hamiltonian):
+    '''
+    <𝜓^∗|𝐻|𝜓>, Calculation system expectation of Hamiltonian in experimental state.
+    para_list: parameters to be optimized
+    qubit_number: qubit number
+    electron_number: electron number
+    Hamiltonian: System Hamiltonian
+    '''
+    fermion_cc =get_ccsd(qubit_number, electron_number, para_list)
+    pauli_cc = JordanWignerTransform(fermion_cc)
+    ucc = cc_to_ucc_hamiltonian(pauli_cc)
+    expectation=0
+    for component in Hamiltonian:
+        expectation+=get_expectation(qubit_number, electron_number, ucc, component)
+    expectation=float(expectation.real)
+    print(expectation)
+    return ("", expectation)
+#获取CCSD模型的参数个数
+def get_ccsd_n_term(qn, en):
+    '''
+    coupled cluster single and double model.
+    e.g. 4 qubits, 2 electrons
+    then 0 and 1 are occupied,just consider 0->2,0->3,1->2,1->3,01->23
+    '''
+
+    if n_electron>n_qubit:
+        assert False
+    
+    return int((qn - en) * en + (qn - en)* (qn -en - 1) * en * (en - 1) / 4)
+#构造普通参数的CCSD模型的哈密顿量
+def get_ccsd(qn, en, para):
+    '''
+    get Coupled cluster single and double model.
+    e.g. 4 qubits, 2 electrons
+    then 0 and 1 are occupied,just consider 0->2,0->3,1->2,1->3,01->23.
+    returned FermionOperator like this:
+    {{"2+ 0":var[0]},{"3+ 0":var[1]},{"2+ 1":var[2]},{"3+ 1":var[3]},
+    {"3+ 2+ 1 0":var[4]}}
+
+    '''
+    if n_electron>n_qubit:
+        assert False
+    if n_electron==n_qubit:
+        return FermionOperator()
+
+    if get_ccsd_n_term(qn, en) != len(para):
+        assert False
+
+    cnt = 0
+    fermion_op = FermionOperator()
+    for i in range(en):
+        for ex in range(en, qn):
+            fermion_op += FermionOperator(str(ex) + "+ " + str(i), para[cnt])
+            cnt += 1   
+    
+    for i in range(n_electron):
+        for j in range(i+1,n_electron):
+            for ex1 in range(n_electron,n_qubit):
+                for ex2 in range(ex1+1,n_qubit):
+                    fermion_op += FermionOperator(
+                        str(ex2)+"+ "+str(ex1)+"+ "+str(j)+" "+str(i),
+                        para[cnt]
+                    )
+                    cnt += 1
+                    
+    return fermion_op
+#JW变换，将FermionOperator转换成PauliOperator
+def JordanWignerTransform(fermion_op):
+    data = fermion_op.data()
+    pauli = PauliOperator()
+    for i in data:
+        pauli += get_fermion_jordan_wigner(i[0][0])*i[1]
+    return pauli
+#JordanWigner变换
+def get_fermion_jordan_wigner(fermion_item):
+    pauli = PauliOperator("", 1)
+
+    for i in fermion_item:
+        op_qubit = i[0]
+        op_str = ""
+        for j in range(op_qubit):
+            op_str += "Z" + str(j) + " "
+
+        op_str1 = op_str + "X" + str(op_qubit)
+        op_str2 = op_str + "Y" + str(op_qubit)
+
+        pauli_map = {}
+        pauli_map[op_str1] = 0.5
+
+        if i[1]:
+            pauli_map[op_str2] = -0.5j
+        else:
+            pauli_map[op_str2] = 0.5j
+
+        pauli *= PauliOperator(pauli_map)
+
+    return pauli
+#CC到UCC变换
+def cc_to_ucc_hamiltonian(cc_op):
+    '''
+    generate Hamiltonian form of unitary coupled cluster 
+    based on coupled cluster,H=1j*(T-dagger(T)),
+    then exp(-iHt)=exp(T-dagger(T))
+    '''
+    return 1j*(cc_op-cc_op.dagger())
+#计算期望
+def get_expectation(n_qubit, n_en, ucc,component):
+    '''
+    get expectation of one hamiltonian.
+    n_qubit: qubit number
+    n_en: electron number
+    ucc: unitary coupled cluster operator
+    component: paolioperator and coefficient,e.g. ('X0 Y1 Z2',0.2)
+    '''
+
+    machine=init_quantum_machine(QMachineType.CPU)
+    q = machine.qAlloc_many(n_qubit)
+    prog=QProg()
+
+    prog.insert(prepareInitialState(q, n_en))
+    prog.insert(simulate_hamiltonian(q, ucc, 1.0, 4))
+    
+    for i, j in component[0].items():
+        if j=='X':
+            prog.insert(H(q[i]))
+        elif j=='Y':
+            prog.insert(RX(q[i],pi/2))
+    
+    machine.directly_run(prog)
+    result=machine.get_prob_dict(q, select_max=-1)
+    machine.qFree_all(q)
+    
+    expectation=0
+    #奇负偶正
+    for i in result:
+        if parity_check(i, component[0]):
+            expectation-=result[i]
+        else:
+            expectation+=result[i]       
+    return expectation*component[1]
+#制备初态
+def prepareInitialState(qlist, en):
+    '''
+    prepare initial state. 
+    qlist: qubit list
+    en: electron number
+    return a QCircuit
+    '''
+    circuit = QCircuit()
+    if len(qlist) < en:
+        return circuit
+
+    for i in range(en):
+        circuit.insert(X(qlist[i]))
+
+    return circuit;
+#哈密顿量模拟
+def simulate_hamiltonian(qubit_list,pauli,t,slices=3):
+    '''
+    Simulate a general case of hamiltonian by Trotter-Suzuki
+    approximation. U=exp(-iHt)=(exp(-i H1 t/n)*exp(-i H2 t/n))^n
+    '''
+    circuit =QCircuit()
+
+    for i in range(slices):
+        for op in pauli.data():
+            term = op[0][0]
+            circuit.insert(
+                simulate_one_term(
+                    qubit_list, 
+                    term, op[1].real, 
+                    t/slices
+                )
+            )
+
+    return circuit
+#哈密顿量模拟——对单个子项进行模拟
+def simulate_one_term(qubit_list, hamiltonian_term, coef, t):
+    '''
+    Simulate a single term of Hamilonian like "X0 Y1 Z2" with
+    coefficient and time. U=exp(-it*coef*H)
+    '''
+    circuit =QCircuit()
+
+    if not hamiltonian_term:
+        return circuit
+
+    transform=QCircuit()
+    tmp_qlist = []
+    for q, term in hamiltonian_term.items():        
+        if term is 'X':            
+            transform.insert(H(qubit_list[q]))            
+        elif term is 'Y':
+            transform.insert(RX(qubit_list[q],pi/2))              
+
+        tmp_qlist.append(qubit_list[q])     
+
+    circuit.insert(transform)
+
+    size = len(tmp_qlist)
+    if size == 1:
+        circuit.insert(RZ(tmp_qlist[0], 2*coef*t))
+    elif size > 1:
+        for i in range(size - 1):
+            circuit.insert(CNOT(tmp_qlist[i], tmp_qlist[size - 1]))   
+        circuit.insert(RZ(tmp_qlist[size-1], 2*coef*t))
+        for i in range(size - 1):
+            circuit.insert(CNOT(tmp_qlist[i], tmp_qlist[size - 1]))  
+
+    circuit.insert(transform.dagger())
+
+    return circuit
+#奇偶校验
+def parity_check(number, terms):
+    '''
+    pairty check 
+    number: quantum state
+    terms: a single term of PauliOperator, like"[(0, X), (1, Y)]"
+    '''
+    check=0
+    number=number[::-1]
+    for i in terms:
+        if number[i]=='1':
+            check+=1
+    return check%2
+#非梯度下降优化算法
+def optimize_by_no_gradient(mol_pauli, n_qubit, n_en, iters):
+    n_para = get_ccsd_n_term(n_qubit, n_electron)
+
+    para_vec = []
+    for i in range(n_para):
+        para_vec.append(0.5)
+
+    no_gd_optimizer = OptimizerFactory.makeOptimizer(OptimizerType.NELDER_MEAD)
+    no_gd_optimizer.setMaxIter(iters)
+    no_gd_optimizer.setMaxFCalls(iters)
+    no_gd_optimizer.registerFunc(partial(
+        loss_func, 
+        qubit_number = n_qubit, 
+        electron_number = n_en,
+        Hamiltonian=mol_pauli.toHamiltonian(1)), 
+        para_vec)
+
+    no_gd_optimizer.exec()
+    result = no_gd_optimizer.getResult()
+    print(result.fun_val)
+
+    return result.fun_val
+#获取原子对应的电子数
+def getAtomElectronNum(atom):
+    atom_electron_map = {
+        'H':1, 'He':2, 'Li':3, 'Be':4, 'B':5, 'C':6, 'N':7, 'O':8, 'F':9, 'Ne':10, 
+        'Na':11, 'Mg':12, 'Al':13, 'Si':14, 'P':15, 'S':16, 'Cl':17, 'Ar':18
+    }
+
+    if (not atom_electron_map.__contains__(atom)):
+        return 0
+
+    return atom_electron_map[atom]
+#主函数
+if __name__=="__main__":    
+    distances = [x * 0.1 for x in range(2, 25)]
+    molecule = "H 0 0 0\nH 0 0 {0}"
+
+    molecules = []
+    for d in distances:
+        molecules.append(molecule.format(d))
+
+    chemistry_dict = {
+        "mol":"",
+        "multiplicity":1,
+        "charge":0,
+        "basis":"sto-3g",
+    }
+
+    energies = []
+
+    for d in distances:
+        mol = molecule.format(d)
+
+        chemistry_dict["mol"] = molecule.format(d)
+        data = run_psi4(chemistry_dict)
+        #get molecule electron number
+        n_electron = 0
+        mol_splits = mol.split()
+        cnt = 0
+        while (cnt < len(mol_splits)):
+            n_electron += getAtomElectronNum(mol_splits[cnt])
+            cnt += 4
+
+        fermion_op = parsePsi4DataToFermion(data[1])
+        pauli_op = JordanWignerTransform(fermion_op)
+
+        n_qubit = pauli_op.getMaxIndex()
+
+        energies.append(optimize_by_no_gradient(pauli_op, n_qubit, n_electron, 200))
+
+    plt.plot(distances , energies, 'r')
+    plt.xlabel('distance')
+    plt.ylabel('energy')
+    plt.title('VQE PLOT')
+    plt.show()

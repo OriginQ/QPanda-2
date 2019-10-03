3a6b78a2a7da711d3d9a89de906c7ad135acbaba
tecog <9999172+tecog@users.noreply.github.com>
2019-10-03 18:16:39 +0800

    Update DJ_Algorithm.cpp

--- Applications/DJ_Algorithm/DJ_Algorithm.cpp
+++ Applications/DJ_Algorithm/DJ_Algorithm.cpp
@@ -14,9 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include "Utilities/Utilities.h"
+#include "Core/Utilities/Utilities.h"
 #include "QAlg/DJ_Algorithm/DJ_Algorithm.h"
-#include "QPandaNamespace.h"
+#include "Core/Utilities/QPandaNamespace.h"
 #include "Core/QPanda.h"
 #include <vector>
 

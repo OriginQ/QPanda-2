3e88940f4e1461a987de37ba0b31be11f4703be5
tecog <9999172+tecog@users.noreply.github.com>
2019-10-03 18:04:52 +0800

    修正了头文件引用路径

--- Applications/B_V_Algorithm/BernsteinVaziraniAlgorithm.cpp
+++ Applications/B_V_Algorithm/BernsteinVaziraniAlgorithm.cpp
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include "Core/QPanda.h"
-#include "Utilities/Utilities.h"
+#include "Core/Utilities/Utilities.h"
 #include "QAlg/B_V_Algorithm/BernsteinVaziraniAlgorithm.h"
 #include <bitset>
 
